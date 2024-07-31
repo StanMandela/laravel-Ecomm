80e4ca4e45dcf958bfc252ba570515991f50d5af
stanMandela <smstan8@gmail.com>
2024-07-31 17:52:57 +0300

    added controller for product

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -35,9 +35,9 @@ class ProductsController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Products $products)
+    public function show(Products $product)
     {
-        //
+        return $product;
     }
 
     /**
